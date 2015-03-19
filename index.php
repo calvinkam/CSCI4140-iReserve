d038f77857faad003bacb0bf771dd42523f60602
Calvin Kam <calvinkam123@gmail.com>
2015-03-20 05:09:39 +0800

    Fix Bugs: Invalid tag names

--- index.php
+++ index.php
@@ -66,15 +66,15 @@ $cap = create_captcha($vals);*/
 			<div class="col-sm-6">
 				<form class="form-horizontal">
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <label for="email" class="col-sm-2 control-label">Email</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      <input type="email" class="form-control" id="email" placeholder="Email">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <label for="password" class="col-sm-2 control-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+      <input type="password" class="form-control" id="password" placeholder="Password">
     </div>
   </div>
   
